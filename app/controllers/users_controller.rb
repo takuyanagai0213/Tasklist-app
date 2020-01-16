1d418e0b5ae04978203beaae8983c92f7589e90c
takuya.nagai <nagataku021@outlook.jp>
2020-01-16 13:37:41 +0000

    Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  # before_action :require_user_logged_in, only: [:index, :show]
+   before_action :require_user_logged_in, only: [:index, :show]
   def index
     @users = User.order(id: :desc)
   end
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
