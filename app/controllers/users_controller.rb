037249afe0b6348dfe872b03a73f69c202e2e150
takuya.nagai <nagataku021@outlook.jp>
2020-01-19 14:21:55 +0000

    for heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  # before_action :require_user_logged_in, only: [:index, :show]
+　before_action :require_user_logged_in, only: [:index, :show]
   def index
     @users = User.order(id: :desc)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  get 'users', to: 'users#index'
   post 'users', to: 'users#create'
   
   resources :tasks
